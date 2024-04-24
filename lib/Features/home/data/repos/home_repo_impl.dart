fa95baaa3a77971ae1b0a52df865b9e1c30eb928
Yasser Mohamed <139704774+YasserMuhamed@users.noreply.github.com>
2024-04-24 07:26:04 +0200

    create featuredBooksCubit

--- lib/Features/home/data/repos/home_repo_impl.dart
+++ lib/Features/home/data/repos/home_repo_impl.dart
@@ -1,18 +1,47 @@
 import 'package:bookly_app/Features/home/data/models/book_model/book_model.dart';
 import 'package:bookly_app/Features/home/data/repos/home_repo.dart';
 import 'package:bookly_app/core/errors/failure.dart';
+import 'package:bookly_app/core/utils/api_service.dart';
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 
 class HomeRepoImpl implements HomeRepo {
+  final ApiService apiService;
+
+  HomeRepoImpl(this.apiService);
   @override
-  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks() {
-    // TODO: implement fetchBestSellerBooks
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() async {
+    try {
+      Map<String, dynamic> data = await apiService.get(
+          'volumes?Filtering=free-ebooks&q=Subject:Programming&Sorting=newest');
+      List<BookModel> books = [];
+      for (var book in data['items']) {
+        books.add(BookModel.fromJson(book));
+      }
+      return right(books);
+    } catch (e) {
+      if (e is DioError) {
+        return left(ServerFailure.fromDioError(e));
+      }
+      return left(ServerFailure(errMessage: e.toString()));
+    }
   }
 
   @override
-  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() {
-    // TODO: implement fetchFeaturedBooks
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchNewestBooks() async {
+    try {
+      Map<String, dynamic> data = await apiService
+          .get('volumes?Filtering=free-ebooks&q=Subject:Programming');
+      List<BookModel> books = [];
+      for (var book in data['items']) {
+        books.add(BookModel.fromJson(book));
+      }
+      return right(books);
+    } catch (e) {
+      if (e is DioError) {
+        return left(ServerFailure.fromDioError(e));
+      }
+      return left(ServerFailure(errMessage: e.toString()));
+    }
   }
 }
--- lib/core/errors/failure.dart
+++ lib/core/errors/failure.dart
@@ -1,3 +1,58 @@
-abstract class Failure {}
+import 'package:dio/dio.dart';
 
-class ServerFailure extends Failure {}
+abstract class Failure {
+  final String errMessage;
+
+  Failure({required this.errMessage});
+}
+
+class ServerFailure extends Failure {
+  ServerFailure({required super.errMessage});
+
+  // ignore: deprecated_member_use
+  factory ServerFailure.fromDioError(DioError dioError) {
+    switch (dioError.type) {
+      case DioExceptionType.connectionTimeout:
+        return ServerFailure(errMessage: "connection timeout with ApiServer");
+      case DioExceptionType.sendTimeout:
+        return ServerFailure(errMessage: "send timeout with ApiServer");
+
+      case DioExceptionType.receiveTimeout:
+        return ServerFailure(errMessage: "Receive timeout with ApiServer");
+
+      case DioExceptionType.badCertificate:
+        return ServerFailure(errMessage: "badCertificate with ApiServer");
+
+      case DioExceptionType.badResponse:
+        return ServerFailure.fromResponse(
+            dioError.response!.statusCode!, dioError.response!.data);
+      case DioExceptionType.cancel:
+        return ServerFailure(errMessage: "Request to ApiServer was canceled");
+
+      case DioExceptionType.connectionError:
+        return ServerFailure(errMessage: "there is internet connection Error");
+      case DioExceptionType.unknown:
+        return ServerFailure(
+            errMessage: "Oops There Was An Error ,Please Try Later");
+
+      default:
+        return ServerFailure(
+            errMessage: "Oops There Was An Error ,Please Try Later");
+    }
+  }
+
+  factory ServerFailure.fromResponse(int statusCode, dynamic response) {
+    if (statusCode == 400 || statusCode == 401 || statusCode == 403) {
+      return ServerFailure(errMessage: response["error"]["message"]);
+    } else if (statusCode == 404) {
+      return ServerFailure(
+          errMessage: "Your Request Not Found, Please Try Later");
+    } else if (statusCode == 500) {
+      return ServerFailure(
+          errMessage: "Internal Server Error, Please Try Later");
+    } else {
+      return ServerFailure(
+          errMessage: "Oops There Was An Error ,Please Try Later");
+    }
+  }
+}
